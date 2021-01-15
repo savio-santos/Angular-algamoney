1fc18793d403ed6a297c814aa5fe81cfaef2b7af
Sávio Santos <savio@smart4sistemas.com>
2021-01-15 11:39:34 -0300

    tornando propriedade interna de um componente acessivel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
-import { BemVindoComponent } from './bem-vindo/bem-vindo.component';
 import { FormsModule } from '@angular/forms';
+import { FuncionarioCardComponent } from './funcionario-card/funcionario-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloComponent,
-    BemVindoComponent
+        FuncionarioCardComponent
   ],
   imports: [
     BrowserModule,
