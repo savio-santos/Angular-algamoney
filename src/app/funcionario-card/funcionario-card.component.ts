79aef3ef8969d88aebe067de22ece1e3d2eb78ec
Sávio Santos <saviosantos276@gmail.com>
2021-02-26 14:12:20 -0300

    stilos css dinamicos

--- src/app/funcionario-card/funcionario-card.component.ts
+++ src/app/funcionario-card/funcionario-card.component.ts
@@ -14,4 +14,11 @@ export class FuncionarioCardComponent {
 
 @Input('obj') funcionario:any; //exportando propriedade
 
+getStylosCartao(){
+  return {
+    borderWidth: `${this.funcionario.id}px` ,
+    backgroundColor:this.funcionario.id % 2 === 0 ? 'lightblue' : 'lightgreen'
+  };
+}
+
 }
